11accc250fb652bc16efbdca59881812fc6e73a3
aarushi353 <aarushiabrol999@gmail.com>
2021-04-17 00:37:44 +0530

    templating of landing page done

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('hackathons/', views.hackathons, name="hackathons"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render,redirect
 
 def home(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def hackathons(request):
+    return render(request, 'hackathons.html')
+
+def register(request):
+    return render(request, 'Register.html')
+

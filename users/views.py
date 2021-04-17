c16c368c9772fe0b5b8e66c3c10cb421f45fb40b
Ishaan-10 <b.ishaan10@gmail.com>
2021-04-17 11:39:05 +0530

    danger

--- users/views.py
+++ users/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 
 
 def home(request):
     return render(request, 'index.html')
 
+
 def login(request):
     return render(request, 'login.html')
+
 
 def hackathons(request):
     return render(request, 'hackathons.html')
 
+
 def register(request):
     return render(request, 'Register.html')
+
 
 def hackers(request):
     return render(request, 'hackers.html')
 
+
+def chat(request):
+    return render(request, 'chat.html')

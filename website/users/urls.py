2dd5599564adfe581befb03d57f5d841c88e7d6d
aarushi353 <aarushiabrol999@gmail.com>
2021-04-17 14:20:49 +0530

    hackers edited again

---
+++ website/users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('hackathons/', views.hackathons, name="hackathons"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('hackers/', views.hackers, name="hackers"),
+    path('chat/', views.chat, name="chat"),
+]

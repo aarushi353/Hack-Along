d8ddc1af1c2933f4045d66127221e4a9652d053a
aarushi353 <aarushiabrol999@gmail.com>
2021-04-18 02:07:09 +0530

    small edits

--- website/website/settings.py
+++ website/website/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'userapp.apps.UserappConfig',
     'users.apps.UsersConfig',
-    'userapp.apps.UserappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

336208320244ad876806b36517a7342236579a56
aarushi353 <aarushiabrol999@gmail.com>
2021-04-17 15:54:24 +0530

    database created

--- website/users/models.py
+++ website/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Hacker(models.Model):
+        name = models.CharField(max_length=100),
+        college = models.CharField(max_length=100),
+        skill_1 = models.CharField(max_length=100),
+        skill_2 = models.CharField(max_length=100),
+        skill_3 = models.CharField(max_length=100),
+        

60a0a5e4a92781051ca6196d1e681bc28fbd9b87
aarushi353 <aarushiabrol999@gmail.com>
2021-04-19 18:03:36 +0530

    updation of profile and image size added

--- website/userapp/forms.py
+++ website/userapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField
@@ -14,8 +15,28 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         help_texts = {
-            'username': None,
-            'password1': None,
-            'password2': None,
-    }
+        'username': None
+        }
         fields = ['username', 'email', 'password1', 'password2', 'skills', 'linkedin', 'github', 'achievements']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    skills = forms.CharField(max_length=100)
+    linkedin = forms.CharField(max_length=100)
+    github = forms.CharField(max_length=100)
+    achievements = forms.CharField(max_length=100)
+    password1 = forms.CharField(max_length=100)
+    password2 = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        help_texts = {
+        'username': None
+        }
+        fields = ['username', 'email', 'password1', 'password2', 'skills', 'linkedin', 'github', 'achievements']
+            
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
+        
